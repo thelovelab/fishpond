2faee2c13f16eac1510a163c878cccdf94da4060
mikelove <michaelisaiahlove@gmail.com>
2018-10-05 19:28:58 -0400

    fix notation

--- R/pond.R
+++ R/pond.R
@@ -1,11 +1,11 @@
-pond <- function(x, y, c) {
-  rep.idx <- seq_along(assayNames(x))
-  keep.mat <- sapply(rep.idx, function(i) rowSums(assays(x)[[i]] >= 10) >= 3)
+pond <- function(y, x, c) {
+  rep.idx <- seq_along(assayNames(y))
+  keep.mat <- sapply(rep.idx, function(i) rowSums(assays(y)[[i]] >= 10) >= 3)
   keep <- apply(keep.mat, 1, all)
-  xs <- x[keep,] # x subset
-  tt <- sapply(rep.idx, function(i) genefilter::rowttests(assays(xs)[[i]], xs[[y]])$statistic)
-  mcols(x)$stat <- numeric(nrow(x))
-  mcols(x)$stat[keep] <- rowMeans(tt)
-  mcols(x)$stat[!keep] <- NA
-  x
+  ys <- y[keep,] # y subset
+  tt <- sapply(rep.idx, function(i) genefilter::rowttests(assays(ys)[[i]], ys[[x]])$statistic)
+  mcols(y)$stat <- numeric(nrow(y))
+  mcols(y)$stat[keep] <- rowMeans(tt)
+  mcols(y)$stat[!keep] <- NA
+  y
 }
