a28f9b2f92b4b99fd268e27dd8690d8b90e12358
mikelove <michaelisaiahlove@gmail.com>
2018-10-05 16:21:33 -0400

    skeleton

--- R/pond.R
+++ R/pond.R
@@ -1,3 +1,11 @@
-pond <- function() {
-  NULL
+pond <- function(x, y, c) {
+  rep.idx <- seq_along(assayNames(x))
+  keep.mat <- sapply(rep.idx, function(i) rowSums(assays(x)[[i]] >= 10) >= 3)
+  keep <- apply(keep.mat, 1, all)
+  xs <- x[keep,] # x subset
+  tt <- sapply(rep.idx, function(i) genefilter::rowttests(assays(xs)[[i]], xs[[y]])$statistic)
+  mcols(x)$stat <- numeric(nrow(x))
+  mcols(x)$stat[keep] <- rowMeans(tt)
+  mcols(x)$stat[!keep] <- NA
+  x
 }
