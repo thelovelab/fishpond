05c6b7acff827df0f83a4c45038a6c3f44ae55a0
mikelove <michaelisaiahlove@gmail.com>
2020-04-04 07:58:04 -0400

    add unit tet

--- tests/testthat/test_readEDS.R
+++ tests/testthat/test_readEDS.R
@@ -2,16 +2,18 @@ context("readEDS")
 library(fishpond)
 test_that("Reading in Alevin EDS format works", {
 
-  dir <- system.file("extdata", package="tximportData")
-  files <- file.path(dir,"alevin/neurons_900_v014/alevin/quants_mat.gz")
+  dir <- system.file("extdata/alevin/neurons_900_v014/alevin", package="tximportData")
+  files <- file.path(dir,"quants_mat.gz")
   file.exists(files)
-  dir <- sub("/alevin$","",dirname(files))  
   barcode.file <- file.path(dir, "alevin/quants_mat_rows.txt")
   gene.file <- file.path(dir, "alevin/quants_mat_cols.txt")
+  tier.file <- file.path(dir,"quants_tier_mat.gz")
   cell.names <- readLines(barcode.file)
   gene.names <- readLines(gene.file)
   num.cells <- length(cell.names)
   num.genes <- length(gene.names)
+
+  # reading in quants
   mat <- readEDS(numOfGenes=num.genes, numOfOriginalCells=num.cells, countMatFilename=files)
 
   expect_equal(nrow(mat), num.genes)
@@ -21,4 +23,8 @@ test_that("Reading in Alevin EDS format works", {
   expect_lte(max(cts), 1e6)
   expect_gte(min(cts), 0)
 
+  # attempt reading in tier file
+  files <- tier.file
+  tier <- readEDS(numOfGenes=num.genes, numOfOriginalCells=num.cells, countMatFilename=files, tierImport=TRUE)
+  
 })
