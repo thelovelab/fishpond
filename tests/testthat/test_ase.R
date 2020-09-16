f3678a4489a06e9ab437c6591c68b1b9290ef4ff
mikelove <michaelisaiahlove@gmail.com>
2020-09-16 17:46:14 -0400

    tweaks

--- tests/testthat/test_ase.R
+++ tests/testthat/test_ase.R
@@ -5,15 +5,16 @@ library(fishpond)
 test_that("swish for ASE works", {
 
   set.seed(1)
-  
-  y <- makeSimSwishData(n=20)
-  y <- scaleInfReps(y, quiet=TRUE)
-  y <- labelKeep(y)
-  y$pair <- rep(1:10,2)
 
-  y <- swish(y, x="condition", pair="pair")
-  hist(mcols(y)$pvalue)
-  z <- mcols(y)$stat
-  hist(z[abs(z)<20], breaks=100)
+  par(mfrow=c(3,1))
+  for (np in c(8,10,12)) {
+    y <- makeSimSwishData(n=np*2)
+    y <- scaleInfReps(y, quiet=TRUE)
+    y <- labelKeep(y)
+    y$pair <- rep(1:np,2)
+    y <- swish(y, x="condition", pair="pair")
+    z <- mcols(y)$stat
+    hist(z[abs(z)<20], breaks=100, xlim=c(-30,30))
+  }
   
 })
