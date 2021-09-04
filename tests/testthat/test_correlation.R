71c3c026076c75b4900fc12e327555cfb62f00e6
mikelove <michaelisaiahlove@gmail.com>
2021-09-03 20:57:55 -0400

    more correlations

--- tests/testthat/test_correlation.R
+++ tests/testthat/test_correlation.R
@@ -6,14 +6,15 @@ test_that("swish can detect correlations with log counts", {
 
   set.seed(1)
 
-  y <- makeSimSwishData(m=500, n=20, null=TRUE)
+  n <- 20
+  y <- makeSimSwishData(m=500, n=n, null=TRUE)
   nms <- c("counts",paste0("infRep",1:20))
-  lambda1 <- exp(seq(4, 5, length.out=20))
+  lambda1 <- exp(seq(4, 5, length.out=n))
   lambda2 <- rev(lambda1)
-  y$condition <- sort(runif(20,0,0.1))
+  y$condition <- sort(runif(n,0,0.1))
   for (a in nms) {
-    assays(y)[[a]][1,] <- rpois(20,lambda1)
-    assays(y)[[a]][2,] <- rpois(20,lambda2)
+    assays(y)[[a]][1,] <- rpois(n,lambda1)
+    assays(y)[[a]][2,] <- rpois(n,lambda2)
   }
   y <- scaleInfReps(y, quiet=TRUE)
   y <- labelKeep(y)
@@ -22,7 +23,7 @@ test_that("swish can detect correlations with log counts", {
   y <- swish(y, x="condition", cor="spearman", nperms=30)
   #hist(mcols(y)$pvalue)
   #plot(-log10(mcols(y)$pvalue[1:30]))
-  mcols(y)[1:2,]
+  mcols(y)[1:4,]
   mcols(y)[1,"log2FC"]
   coef(lm(log2(lambda1) ~ y$condition))[[2]]
   #plotInfReps(y, idx=1, x="condition")
@@ -30,25 +31,24 @@ test_that("swish can detect correlations with log counts", {
   y <- swish(y, x="condition", cor="pearson", nperms=30)
   #hist(mcols(y)$pvalue)
   #plot(-log10(mcols(y)$pvalue[1:30]))
-  mcols(y)[1:2,]
+  mcols(y)[1:4,]
 
-  # correlation with a log fold change
+  ### correlation with a log fold change ###
   
   set.seed(5)
 
-  y <- makeSimSwishData(m=500, n=20, null=TRUE)
+  y <- makeSimSwishData(m=500, n=n, null=TRUE)
   nms <- c("counts",paste0("infRep",1:20))
-  lambda1 <- exp(seq(4, 5, length.out=10))
+  lambda1 <- exp(seq(4, 5, length.out=n/2))
   lambda2 <- rev(lambda1)
   ar <- log2(lambda1/lambda2) # the allelic ratio for gene 1
-  y$condition <- factor(rep(c("a2","a1"),each=10), levels=c("a2","a1"))
-  y$pair <- rep(1:10,2)
-  cov <- sort(runif(10,0,0.1)) # the numeric covariate
+  y$condition <- factor(rep(c("a2","a1"),each=n/2), levels=c("a2","a1"))
+  y$pair <- rep(1:(n/2),2)
+  cov <- sort(runif(n/2,0,0.1)) # the numeric covariate
   y$cov <- rep(cov,2)
-  plot(y$cov[1:10], ar)  
   for (a in nms) {
-    assays(y)[[a]][1,] <- rpois(20,c(lambda2,lambda1))
-    assays(y)[[a]][2,] <- rpois(20,c(lambda1,lambda2))
+    assays(y)[[a]][1,] <- rpois(n,c(lambda2,lambda1))
+    assays(y)[[a]][2,] <- rpois(n,c(lambda1,lambda2))
   }
 
   y <- scaleInfReps(y, quiet=TRUE)
@@ -59,15 +59,41 @@ test_that("swish can detect correlations with log counts", {
              cov="cov", cor="pearson", nperms=30)
   #hist(mcols(y)$pvalue)
   #plot(-log10(mcols(y)$pvalue[1:30]))
-  mcols(y)[1:2,]
+  mcols(y)[1:4,]
 
   y <- swish(y, x="condition", pair="pair",
              cov="cov", cor="spearman", nperms=30)
   #hist(mcols(y)$pvalue)
   #plot(-log10(mcols(y)$pvalue[1:30]))
-  mcols(y)[1:2,]
+  mcols(y)[1:4,]
   
   #plotInfReps(y, 1, x="cov", cov="condition",
   #            legend=TRUE, legendPos="top")
-    
+
+  ### up-down-up pattern ###
+
+  set.seed(1)
+
+  n <- 40
+  y <- makeSimSwishData(m=500, n=n, null=TRUE)
+  nms <- c("counts",paste0("infRep",1:20))
+  cov <- rep(1:(n/4),each=2)
+  lambda1 <- (cov - 1)*(cov - 10)*(cov - 10) + 100
+  lambda2 <- -1*(cov - 1)*(cov - 1)*(cov - 10) + 100
+  ar <- log2(lambda1/lambda2) # the allelic ratio for gene 1
+  y$condition <- factor(rep(c("a2","a1"),each=n/2), levels=c("a2","a1"))
+  y$pair <- rep(1:(n/2),2)
+  y$cov <- rep(cov,2)
+  for (a in nms) {
+    assays(y)[[a]][1,] <- rpois(n,c(lambda2,lambda1))
+    assays(y)[[a]][2,] <- rpois(n,c(lambda1,lambda2))
+  }
+
+  y <- scaleInfReps(y, quiet=TRUE)
+  y <- labelKeep(y)
+  y <- computeInfRV(y)
+  
+  #plotInfReps(y, 1, x="cov", cov="condition",
+  #            legend=TRUE, legendPos="top")  
+  
 })
